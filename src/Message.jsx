0e98f6020a119831d80ec688860d13e8c51f0927
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2019-08-04 07:45:05 +0300

    Using static defaultProps to set the default property values

--- src/Message.jsx
+++ src/Message.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class Message extends React.Component {
+  static defaultProps = {
+    text: "Hello World from React Native with a text property and default value (Message is a class component)"
+  }
+
   render() {
     return (
       <div>
